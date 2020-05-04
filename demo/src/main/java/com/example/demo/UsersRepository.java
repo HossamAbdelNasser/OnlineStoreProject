4825ab1a5bcb9ea5ac998ff1c63b8b933695efdb
HossamAbdelNasser <hossamabdelnasser9@gmail.com>
2020-05-04 09:40:39 +0200

    HossamAbdelNasser/Updates

--- demo/src/main/java/com/example/demo/UsersRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo;
-import java.util.Optional;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsersRepository extends JpaRepository<Users,Integer> {
-
-	Optional<Users> findByUsername(String username);
-}
--- demo/src/main/java/com/example/demo/users/UserResources.java
+++ demo/src/main/java/com/example/demo/users/UserResources.java
@@ -25,4 +25,6 @@ public class UserResources {
 		usersRepository.save(users);
 		return usersRepository.findAll(); 
 	}
+	
+	
 }
